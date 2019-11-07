87567c5c89ef23720145982243243400fff5397a
Ramiro Andrés <iamramiroo@gmail.com>
2019-11-06 22:42:38 -0500

    Updates

---
+++ precache-manifest.187e0f2cc2847e7a29d091e84fc00d08.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fd31f860b97a79823273ffe46f5416",
+    "url": "/my-todo-list/index.html"
+  },
+  {
+    "revision": "9f502ee00a8ff4c712ed",
+    "url": "/my-todo-list/static/js/2.9cf8d823.chunk.js"
+  },
+  {
+    "revision": "ef675eb1bedfa8d526d1",
+    "url": "/my-todo-list/static/js/main.74d0bf04.chunk.js"
+  },
+  {
+    "revision": "ba4d8b227085adee5d50",
+    "url": "/my-todo-list/static/js/runtime-main.65d7b353.js"
+  }
+]);
