d7ccc600222033fee43b16871dd7e0d3b2f70650
Ramiro Bedoya <ramiro.bedoya-ext@jci.com>
2019-11-12 14:11:56 +0000

    add clock

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CreateTodo from './containers/CreateTodo'
 import Table from './containers/Table'
+import Clock from 'react-clock';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container" style={{ marginTop: "3em" }}>
-        <h1>MY FUCKING TODO LIST</h1>
-        <strong>With react+redux</strong>
-        <div className="row"  style={{ marginTop: "3em" }}>
-          <div className="col-lg-10 offset-lg-2 col-md-10 col-sm-12 col-xs-12">
-            <CreateTodo />
+class App extends Component {
+  state = {
+    date: new Date(),
+  }
+
+  componentDidMount() {
+    setInterval(
+      () => this.setState({ date: new Date() }),
+      1000
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container" style={{ marginTop: "3em" }}>
+          <h1>MY FUCKING TODO LIST</h1>
+          <strong>With react+redux</strong>
+          <div className="row" style={{ marginTop: "3em" }}>
+            <div className="col-lg-10 offset-lg-2 col-md-10 col-sm-12 col-xs-12">
+              <Clock
+                value={this.state.date}
+              />
+            </div>
           </div>
+          <div className="row" style={{ marginTop: "3em" }}>
+            <div className="col-lg-10 offset-lg-2 col-md-10 col-sm-12 col-xs-12">
+              <CreateTodo />
+            </div>
 
+          </div>
+          <Table />
         </div>
-        <Table />
-      </div>
 
-    </div>
-  );
+      </div>
+    );
+  }
+
 }
 
 export default App;
